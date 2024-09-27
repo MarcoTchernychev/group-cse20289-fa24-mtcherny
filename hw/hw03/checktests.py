0dbb0f3a5497cba6f818e97575031c0de40e903c
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-09-26 20:26:15 -0400

    HW03 - filtered the data (task 3.2)

--- hw/hw03/checktests.py
+++ hw/hw03/checktests.py
@@ -3,6 +3,7 @@
 import argparse
 import requests
 import json
+import numpy as np
 
 #INPUT: url as a string
 #OUTPUT: JSON file as a list
@@ -22,16 +23,49 @@ def filterSort(data):
     filtered = [entry for entry in data if (entry["direction"]=="downlink" and entry["type"]=="iperf")]
     filteredsorted =  sorted(filtered, key=lambda x:x["timestamp"])
     return filteredsorted
+#INPUT: JSON file as a list with unfiltered values, and parameters for what you want to filter for
+#OUPUT: JSON file as a list with entries that conatin the values filtered for
+#PURPOSE: return a list of all data points that satisfy the requested filtering (ie. that include the parameters)
+def filterit(data, month = 5, year = 2024, interface = "eth0"):
+    if month>9: #accounting for the fact that one digit integers are written as "05"
+        return list(filter(lambda entry: (entry["interface"]==interface and entry["timestamp"][:4]==str(year) and entry["timestamp"][5:7]==str(month)), data))
+    else:
+        return list(filter(lambda entry: (entry["interface"]==interface and entry["timestamp"][:4]==str(year) and entry["timestamp"][6:7]==str(month)), data))
+#INPUT: JSON file as a list and interface to analyze (eth0 or wlan0)
+#OUTPUT: dictionary for stats for that dataset
+#PURPOSE: take in data points and for a specific interface find data (specified on github)
+def analyze(data, interface):
+    data = list(filter(lambda entry: entry["Interface"] == interface, data))
+    tput_list = [entry["tput_mbps"] for entry in data]
+    dict =  {"Period": data[-1]["timestamp"] - data[0]["timestamp"], 
+             "Interface" : interface, 
+             "Num Points": len(data), 
+             "Min": min(tput_list), 
+             "Max": max(tput_list), 
+             "Mean": mean(tput_list), 
+             "Median": median(tput_list), 
+             "Std Dev": stdev(tput_list), 
+             "10th Percentile": np.percentile(tput_list,10), 
+             "90th Percentile": np.percentile(tput_list,90)}
 
+###START OF FETCHING###
 parser = argparse.ArgumentParser()
 parser.add_argument("url", type=str)
 args = parser.parse_args()
 url = args.url
+###START OF FILTERING###
 data = fetch(url)
-#filterSort(data)
+fsdata = filterSort(data)
+filtered = filterit(fsdata)
 
 ###TESTS###
 #print(type(data)) #list
 #print(data) #list
 #data_string = json.dumps(data, indent = 4)
 #print(data_string) #nice looking string
+#print("\n############################################\n")
+#fsdata_string = json.dumps(fsdata, indent = 4)
+#print(fsdata_string)
+#print("\n############################################\n")
+#filtered_string = json.dumps(filtered, indent = 4)
+#print(filtered_string)
