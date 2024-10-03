ed94c89969a8a5483cdbf8148ba2634da30491ee
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-10-03 08:38:10 -0400

    HW04 - first draft of task 5 done

--- hw/hw04/gogo.py
+++ hw/hw04/gogo.py
@@ -4,7 +4,9 @@ import argparse
 import yaml
 import json
 from functools import reduce
-import checktests.py
+import checktests
+from spire.doc import *
+from spire.doc.common import *
 
 #INPUT: a YAMLfile name as str
 #OUTPUT: a dictionary of tasks where each task has an identifier, URL, year, month, filename for starting text, and identifier placed at start of name for any output file in the task
@@ -23,7 +25,20 @@ def parseYAML(YAMLfile):
         except: #if you can't access all keys, return none
             return None
     return YAMLdict #if you made it past all checks return the dict
-
+#INPUT: dictionary of one task
+#OUTPUT: returns nothing but outputs a pdf that has the same content as the word document from hw3, also, print whne each task is done and when all tasks are done
+#PURPOSE: print the pdf in a concise way
+def dataToPDF(taskDict):
+    checktests.doItAll(taskDict["Year"], taskDict["Month"], taskDict["StartText"], taskDict["URL"], taskDict["Prepend"])
+    document1 = Document()
+    document1.LoadFromFile(f"{task["Prepend"]}{task["Year"]}-{task["Month"]}-Wired.docx")
+    document1.SaveToFile(f"{task["Prepend"]}{task["Year"]}-{task["Month"]}-Wired.docx", FileFormat.PDF)
+    document1.Close()
+    document2 = Document()
+    document2.LoadFromFile(f"{task["Prepend"]}{task["Year"]}-{task["Month"]}-WiFi.docx")
+    document2.SaveToFile(f"{task["Prepend"]}{task["Year"]}-{task["Month"]}-WiFi.docx", FileFormat.PDF)
+    document2.Close()
+    print(f"Task {taskDict} Done!")
 
 #PARSING ARGS
 parser = argparse.ArgumentParser()
@@ -31,5 +46,9 @@ parser.add_argument("YAMLfile", type=str)
 args = parser.parse_args()
 YAMLfile = args.YAMLfile
 
+tasks_dict = parseYAML(YAMLfile)
 #TESTING
-print(json.dumps(parseYAML(YAMLfile), indent=4))
+print(json.dumps(tasks_dict, indent=4))
+for task in tasks_dict:
+    dataToPDF(tasks_dict[task])
+print(f"Completed {len(tasks_dict)} task(s)!")
