b8cc10553ad74cb5042a2e765eda0649aed668d3
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-11-05 12:35:04 -0500

    HW07 - started on ae.sh, just need to extract tar and tar.gz, also previous commits should have been prepended with HW07 - my bad

--- hw/hw07/ae.sh
+++ hw/hw07/ae.sh
@@ -1,7 +1,34 @@
+#ae.sh
 #Alicia Melotik
 #amelotik.nd.edu
 #Marco Tchernychev
 #mtcherny@nd.edu
 
-#!/bin/sh
+#!/bin/bash
+#set -x
+#-o returns the match, '\.' looks for a period, then it takes evrything after the period until the end of the line
+#sed, using 's', subs out what's in the first // (which is a leading period), for what is the second //, which is nothing, so take out the leading period /^\./ --> //
+filetype=$(echo "$1" | grep -o '\..*$' | sed 's/^\.//')
+#echo "$filetype"
+isarchives=$(echo $(ls) | grep -o -w 'archives')
+#echo "$isarchives"
+if [ "$isarchives" != archives ]; then
+    echo "archive directory is not present .. creating!"
+    mkdir archives
+else
+    echo "archive directory already present - no need to create"
+fi
 
+case $filetype in
+    zip)
+        echo "Extracting a zip file via unzip"
+        unzip "$1" -d ./archives
+        ;;
+    tar)
+        echo "Extracting a tar file"
+        tar -x "$1" ./archives
+        ;;
+    tar.gz)
+        echo "Extracting a tar.gz file"
+        ;;
+esac
--- hw/hw07/sbs.sh
+++ hw/hw07/sbs.sh
@@ -1,2 +1,8 @@
 #sbs.sh
+#Alicia Melotik
+#amelotik.nd.edu
+#Marco Tchernychev
+#mtcherny@nd.edu
+
+#!/bin/bash
 
