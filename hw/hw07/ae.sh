715571b358ad5220aa3cb22acd46f7d5ba2a5ec7
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-11-05 13:05:51 -0500

    HW07 - finished ae.sh, added executable access to sbs.sh

--- hw/hw07/ae.sh
+++ hw/hw07/ae.sh
@@ -6,29 +6,35 @@
 
 #!/bin/bash
 #set -x
+
 #-o returns the match, '\.' looks for a period, then it takes evrything after the period until the end of the line
 #sed, using 's', subs out what's in the first // (which is a leading period), for what is the second //, which is nothing, so take out the leading period /^\./ --> //
 filetype=$(echo "$1" | grep -o '\..*$' | sed 's/^\.//')
-#echo "$filetype"
-isarchives=$(echo $(ls) | grep -o -w 'archives')
-#echo "$isarchives"
-if [ "$isarchives" != archives ]; then
+
+#check to see if archive is in the directory
+isarchive=$(echo $(ls) | grep -o -w 'archive')
+
+#if the grep expression didn't return "archive" make the directory and show the message
+if [ "$isarchive" != archive ]; then
     echo "archive directory is not present .. creating!"
-    mkdir archives
+    mkdir archive
+#if the grep expression did return archive, the directory is present, so display the message
 else
     echo "archive directory already present - no need to create"
 fi
 
+#based on the file type display the appropiate message and zip/extract accordingly into the archive dir
 case $filetype in
     zip)
         echo "Extracting a zip file via unzip"
-        unzip "$1" -d ./archives
+        unzip -q "$1" -d ./archive
         ;;
     tar)
         echo "Extracting a tar file"
-        tar -x "$1" ./archives
+        tar -xf "$1" -C ./archive 2>/dev/null
         ;;
     tar.gz)
         echo "Extracting a tar.gz file"
+        tar -xzf "$1" -C ./archive 2>/dev/null
         ;;
 esac
