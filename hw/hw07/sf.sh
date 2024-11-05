417f14fed7b73b07ee1068255d76464a9f39b6c2
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-11-05 13:50:57 -0500

    HW07 - made the txt file and started on sf.sh

--- hw/hw07/sf.sh
+++ hw/hw07/sf.sh
@@ -6,3 +6,17 @@
 
 #!/bin/bash
 #set -x
+
+#check to see that filebeing passed is a txt file
+filetype=$(echo "$1" | grep -o '\..*$' | sed 's/^\.//')
+echo "$filetype"
+if [ "$filetype" != txt ]; then
+    echo "not a txt file"
+fi
+
+#check to see if the exists in the sensitive dir (need to work on this)
+file=$(echo $(ls ./sensitive/) | grep -o -w '"$1"')
+echo "$file"
+if [ "$file" != "$1" ]; then
+    echo "$1 does not exist in sensitive"
+fi
