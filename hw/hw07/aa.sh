cf5eac3da4ca770de174e9b94db78a2674b49a4f
Alicia Melotik <amelotik@student11.cse.nd.edu>
2024-11-06 22:41:01 -0500

    HW07 - started work on aa.sh

---
+++ hw/hw07/aa.sh
@@ -0,0 +1,50 @@
+#aa.sh
+#Alicia Melotik
+#amelotik@nd.edu
+#Marco Tchernychev
+#mtcherny@nd.edu
+
+#!/bin/bash
+set -x
+
+#check if archive file provided as argument
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <file-to-extract> <badsites-file>"
+	exit -1
+fi
+if [ ! -f "$1" ]; then
+	echo "Error: archive file to extract $1 could not be found"
+	exit -1
+fi
+if [ ! -f "$2" ]; then
+	echo "Error: bad sites file $2 could not be found"
+	exit -1
+fi
+
+#first, extract the archive and find files
+sh ae.sh "$1" >/dev/null
+files=$(find ./archive -maxdepth 1 -type f)
+
+#next, check the extracted files for bad urls
+#then check for sensitive info
+for f in $files
+do
+	echo $f
+	badResult=$(./sbs.sh $2 $f)
+	if [[ $? -ne 0 ]]; then
+		echo "$badResult"
+		rm -r archive/
+		exit -2
+	fi
+	sensitiveResult=$(./sf.sh $f)
+	if [[ $? -ne 0 ]]; then
+		echo "$sensitiveResult" | tail -n1
+		rm -r archive/
+		exit -2
+	fi
+done
+
+#if nothing was triggered, echo CLEAN and delete archive directory
+echo "CLEAN"
+rm -r archive/
+
--- hw/hw07/sbs.sh
+++ hw/hw07/sbs.sh
@@ -1,6 +1,6 @@
 #sbs.sh
 #Alicia Melotik
-#amelotik.nd.edu
+#amelotik@nd.edu
 #Marco Tchernychev
 #mtcherny@nd.edu
 
@@ -9,30 +9,30 @@
 
 #check for proper number of arguments
 if [ $# -ne 2 ]; then
-	echo "Incorrect number of arguments. Usage: <badSites.csv> <file>"
+	echo "Incorrect number of arguments. Usage: $0 <badSites.csv> <file>"
 	exit -1
 fi
 
 #check if the bad site list file exists
 if [ ! -f "$1" ]; then
     echo "$1 doesn't exist"
-    exit -2
+    exit -1
 fi
 #check if bad sites file is .csv
-if echo "$2" | grep -qv '\.csv$'; then
+if echo "$1" | grep -qv '\.csv$'; then
 	echo "bad site file must be a .csv"
-	exit -2
+	exit -1
 fi
 
 #check if the file being checked exists
 if [ ! -f "$2" ]; then
     echo "$2 doesn't exist"
-    exit -2
+    exit -1
 fi
 
 ########REPLACE $3 with FOUND URL COLUMN?#########
 #extract bad urls as list from csv
-urls=$(awk -F ',' '{ print $3}' "$2")
+urls=$(awk -F ',' ' NR > 1 { print $3}' "$1")
 
 #read in the arg file and check each line with the bad url list
 while read f; do
@@ -41,10 +41,10 @@ while read f; do
 		#search f for each i
 		if echo "$f" | grep -q "$i"; then
 			echo "MALICIOUSURL: $i"
-			exit -3
+			exit -2
 		fi
 	done
-done < $1
+done < $2
 
 #if no bad urls matched with any of the lines, echo clean
 echo "CLEAN"
