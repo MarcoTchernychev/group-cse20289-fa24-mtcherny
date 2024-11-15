f8afbbf837871f6f32b902a6c3b0f748f9d8ca33
Alicia Melotik <amelotik@student11.cse.nd.edu>
2024-11-15 14:04:52 -0500

    HW08 - testing and debugging scanner.sh

--- hw/hw08/scanner.sh
+++ hw/hw08/scanner.sh
@@ -14,61 +14,68 @@ if [ $# -ne 5 ]; then
 fi
 
 #check that all args exist
-if [ ! -e $1]; then echo "$1 does not exist"; exit 1; fi
-if [ ! -e $2]; then echo "$2 does not exist"; exit 1; fi
-if [ ! -e $3]; then echo "$3 does not exist"; exit 1; fi
-if [ ! -e $4]; then echo "$4 does not exist"; exit 1; fi
-if [ ! -e $5]; then echo "$5 does not exist"; exit 1; fi
+if [ ! -e $1 ]; then echo "$1 does not exist"; exit 1; fi
+if [ ! -e $2 ]; then echo "$2 does not exist"; exit 1; fi
+if [ ! -e $3 ]; then echo "$3 does not exist"; exit 1; fi
+if [ ! -e $4 ]; then echo "$4 does not exist"; exit 1; fi
+if [ ! -e $5 ]; then echo "$5 does not exist"; exit 1; fi
 
 logname="$(date +%F).log"
 #add to log time when script started
 echo "Script started: $(date)" >> "$4/$logname"
 #catch ctrl-c signal using trap, add time to log, then exit
-trap {echo "Script exited: $(date)" >> "$4/$logname"; exit 0} SIGINT
+trap 'echo "Script exited: $(date)" >> "$4/$logname"; exit 0' SIGINT
 
 #starting inf loop
 while true; do    
-    archives=$(ls ./scandata/toscan) #getting list of archives to process in toscan dir
  
 	#for each archive found in the directory, extract its files and then check that
 	#each file within the archive is clean before adding to proper directory and log
-	for archive in $(archives); do
-		extractoutput=$(sh ae.sh "$archive")
+	for archive in $(ls $1); do
 		reasonfile="$archive.reason"
+		extractoutput=$(sh ae.sh "$1$archive")
 
-        if [ $? -e 0 ]; then #check that archive is valid (only need to do once)
+        if [ $? -ne 0 ]; then #check that archive is valid (only need to do once)
             #move to quarantine with reason cannot extract, along with .reason file
 			echo -e "$archive\nCANNOTEXTRACT" > "$reasonfile"
-			mv "$archive" "$3"
+			mv "$1/$archive" "$3"
 			mv "$reasonfile" "$3"
 			echo  "$(date), $archive, QUARANTINE, CANNOT EXTRACT" >> "$4/$logname"
 			continue
-        fi        
- 
-    	for file in $(ls ./archive); do #looping over the files        
+        fi   
+
+    	for file in $(find ./archive -type f); do #looping over the files        
        		urloutput=$(sh sbs.sh $5 "$file") #check for malicious url        
         	sensitiveoutput=$(sh sf.sh "$file") #check for sensitive info       
 
-        	if [[ sensitiveoutput == CLEAN && urloutput==CLEAN ]]; then #if both pass add file to approved 
-            	#move to approved
-				echo  "$(date), $archive, APPROVE" >> "$4/$logname"
-				mv "$archive" "$2"
+        	if [[ sensitiveoutput == CLEAN && urloutput == CLEAN ]]; then #if both pass, check rest of files 
+				continue
         	elif [[ sensitiveoutput != CLEAN ]]; then #if contains sensitive info move to quarantine
             #move to quanrantine along with .reason file
-				reason="$(tail -n 1 "$sensitiveoutput")"
+				reason="$(echo "$sensitiveoutput" | tail -n 1)"
 				echo -e "$file\nSENSITIVE\n$reason" > "$reasonfile"
 				echo  "$(date), $archive, QUARANTINE, SENSITIVE, $reason" >> "$4/$logname"
-				mv "$file" "$3"
-				mv "$reasonfile" "$3"	
+				mv "$1/$archive" "$3"
+				mv "$reasonfile" "$3"
+				break	
         	elif [[ urloutput != CLEAN ]]; then #if contains malicious url move to quarantine
             	#move to quanrantine along with .reason file
-				reason="$(tail -n 1 "$urloutput")"
+				reason="$(echo "$urloutput" | tail -n 1)"
 				echo -e "$file\nMALICIOUSURL\n$reason" > "$reasonfile"
 				echo  "$(date), $archive, QUARANTINE, MALICIOUSURL, $reason" >> "$4/$logname"
-				mv "$file" "$3"
+				mv "$1/$archive" "$3"
 				mv "$reasonfile" "$3"
+				break
         	fi
     	done
+		if [[ sensitiveoutput == CLEAN && urloutput == CLEAN ]]; then 
+			echo  "$(date), $archive, APPROVE" >> "$4/$logname"
+			mv "$1/$archive" "$2"
+		fi
+		#delete archive directory if it exists so files not from curr archive do not get scanned 
+		if [ -e "./archive" ]; then
+			rm -r "./archive"
+		fi
 	done
     sleep 1
 done
