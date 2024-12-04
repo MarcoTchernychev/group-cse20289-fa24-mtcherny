96bd2f7e54148fb06f7fbdfeade74e9e01800fd8
Marco Tchernychev <mtcherny@student12.cse.nd.edu>
2024-12-04 17:48:07 -0500

    HW10 - made changes to theServer

--- hw/hw10/solution/server/theServer.py
+++ hw/hw10/solution/server/theServer.py
@@ -12,26 +12,37 @@ import zmq
 #OUTPUT: true or false, and prints if bad
 #PURPOSE: take in a url and give a JSON file, if it can't give a JSON file it gives back an error
 def checkCmnds(message):
-    validStat = re.compile(r'(count|mean|median|min|max|stddev|list)') #confused about how exit and more will be sent
-    validDate = re.compile(r'')
-    validTime = re.compile(r'[0-23]|\*')
-    validFilter = re.compile(r'iface=(eth0|wlan0);dir=(downlink|uplink);type=iperf')
+    validStat = re.compile(r'^(count|mean|median|min|max|stddev|list)$')
+    validDate = re.compile(r'^[2010-2024]-[0-12]-[0-31]|\*|$')
+    validTime = re.compile(r'^[0-23]|\*$')
+    validFilter = re.compile(r'^iface=(eth0|wlan0);dir=(downlink|uplink);type=iperf$')
     commands = message.split(', ')
     if len(commands) == 1:
         if commands[0] == "exit" or commands[0] == "more":
             return True
+        else:
+            return False
     elif len(commands) == 4:
         stat = commands[0]
-        data = commands[1]
+        date = commands[1]
         time = commands[2]
         filter = commands[3]
-        if stat
+        if not re.fullmatch(validStat, stat):
+            print("Invalid stat argument")
+            return False
+        if not re.fullmatch(validDate, date):
+            print("Invalid date argument")
+            return False
+        if not re.fullmatch(validTime, time):
+            print("Invalid time argument")
+            return False
+        if not re.fullmatch(validFilter, filter):
+            print("Invalid filter argument")
+            return False
+        return True
     else:
         print("Not enough arguments")
         return False
-    
-
-
 
 url = sys.argv[1]
 serverPort = int(sys.argv[2]) #40645
