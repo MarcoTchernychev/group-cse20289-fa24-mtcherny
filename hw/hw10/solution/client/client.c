ef34cedc7eccadd518fd74f74258f684489bc714
Alicia Melotik <amelotik@student12.cse.nd.edu>
2024-12-06 15:25:27 -0500

    HW10 - fix client.c up

--- hw/hw10/solution/client/client.c
+++ hw/hw10/solution/client/client.c
@@ -14,20 +14,19 @@
 
 int main(int argc, char *argv[]) {
 	
-	char *hostname;
+	char hostname[20];
 	int portnum;
 	int rc;
-	if (argc == 1) && (strcmp(argv[1], "exit") == 0) {
-		//exit on python server then here
-
-		exit(0);
-	} else if (argc == 3) {//ADD CORRECT ARG #
+	
+	if (argc == 3) {
 		strcpy(hostname, argv[1]);
-		if (isdigit(argv[2])) {
-			portnum = atoi(argv[2]);
-		} else { //incorrect second arg for port number
-			
-		}	
+		for (int i = 0; argv[2][i] != '\0'; i++) {
+			if (!isdigit(argv[2][i])) {
+				printf("Error: port must be an integer number\n");
+				exit(-1);
+			}
+		}
+		portnum = atoi(argv[2]);
 	}
 	
 	//connect to port via zmq
@@ -49,26 +48,35 @@ int main(int argc, char *argv[]) {
 	char msg[MAX_MSG_LEN];
 	
 	char *token;
+	char temp[MAX_MSG_LEN];
 	const char *delim = ", ";
-	int count = 1;
+	int count;
 	
 	while(1) {
 		//get input from user
 		printf("Waiting for message (stat, date, time, filter)/list/more:\n");	
 		fgets(msg, MAX_MSG_LEN, stdin);
 		//remove trailing newline
-		msg[strlen(msg)-1] = "\0";
+		msg[strlen(msg)-1] = '\0';
+
+		if (strcmp(msg, "exit") == 0) {
+			zmq_send(requester, msg, strlen(msg), 0);
+			exit(0);
+		}
 
 		//check if input is valid (valid # of args/csv type format)
-		if (strcmp(msg, "list") != 0) && (strcmp(msg, "more") != 0) {
+		if ((strcmp(msg, "list") != 0) && (strcmp(msg, "more") != 0)) {
 			//use strtok to see how many fields were entered seperated by commas (proper format)
-			token = strtok(msg, delim);
+			strcpy(temp, msg);
+			token = strtok(temp, delim);
+			count = 0;
 			while (token != NULL) {
 				token = strtok(NULL, delim);
 				count++;
 			}
 			if (count != 4) {
-				printf("Message formatted incorrectly"
+				printf("Message formatted incorrectly\n");
+				continue;
 			}
 		}
 
@@ -81,11 +89,11 @@ int main(int argc, char *argv[]) {
 	
 		//wait for response and then display it
 		memset(response, 0, MAX_MSG_LEN);
-		if (zmq_recv(requester, response, 20, 0) == -1) {
+		if (zmq_recv(requester, response, MAX_MSG_LEN, 0) == -1) {
 			printf("Failed to receive response\n");
 			continue;
 		}
-		printf("Recieved: %s\n", response);	
+		printf("Received: %s\n", response);	
 	
 	}
 
