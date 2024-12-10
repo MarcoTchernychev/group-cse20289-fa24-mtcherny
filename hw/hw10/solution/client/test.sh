21506ce16b3ee741c8730141f1d15fd90771a783
Alicia Melotik <amelotik@student12.cse.nd.edu>
2024-12-10 18:33:15 -0500

    HW10 - finish test.sh

--- hw/hw10/solution/client/test.sh
+++ hw/hw10/solution/client/test.sh
@@ -19,31 +19,44 @@ outputc=$(./client "$hostname")
 if [[ "$outputc" == "Usage: ./client hostname portnumber" ]]; then
 	echo "Successfully detected incorrect number of args"
 fi
-# exit code -2
+
 
 outputc=$(./client "$hostname" "number")
 if [[ "$outputc" == "Error: port must be an integer number" ]]; then
 	echo "Successfully detected non-integer port number arg"
 fi
 
-# $? is exit value of last cmd run
-#o exit code -1
+#run client with assortment of test cases and capture all output
+outputc=$(echo -e "count, *-*-*, 02\nmean, 2024-05-*, *, iface=eth0;dir=uplink;type=iperf\nincorrect, input\nincorrect input\nexit\n" | ./client "$hostname" "$portnum")
 
-python3 ../server/theServer.py "http://ns-mn1.cse.nd.edu/cse20289-fa24/hw03/data-all.json" "$portnum" &
+#check if first commant (count) ran successfully
+if [[ $(echo "$outputc" | head -n 5) ==  "Connecting to server on port $portnum
+	Successfully connected on port $portnum
+Waiting for message (stat, date, time, {filter})/more/exit:
+Sending message: count, *-*-*, 02
+Received: success, count, 189" ]]; then
+	echo "C client successfully outputed count of 189 records"
+fi
 
-echo -e "count, *-*-*, 02\nexit\n" | ./client "$hostname" "$portnum" 
+#check if next cmd (mean w/ specified filter) ran successfully
+if [[ $(echo "$outputc" | sed -n '6,8p') == "Waiting for message (stat, date, time, {filter})/more/exit:
+Sending message: mean, 2024-05-*, *, iface=eth0;dir=uplink;type=iperf
+Received: success, mean, 23.36637723954102" ]]; then
+	echo "C client successfully outputed mean of 23.26..."
+fi
 
-echo -e "incorrect, input\nexit\n" | ./client "$hostname" "$portnum"
+#check if next command (invalid # of input args) ran successfully
+if [[ $(echo "$outputc" | sed -n '9,10p') == "Waiting for message (stat, date, time, {filter})/more/exit:
+Message formatted incorrectly" ]]; then
+	echo "C client successfully caught incorrect number of input commands"
+fi
 
-echo -e "incorrect input\nexit\n" | ./client "$hostname" "$portnum"
+#check if final command (invalid single command) ran successfully
+if [[ $(echo "$outputc" | tail -n 3) == "Waiting for message (stat, date, time, {filter})/more/exit:
+Message formatted incorrectly
+Waiting for message (stat, date, time, {filter})/more/exit:" ]]; then
+	echo "C client successfully caught invalid input command"
+fi
 
-#outputc=$(./client "$hostname" "$portnum" | tee temp_c.txt &)
-#echo $outputc
-#"\tSuccessfully connected on port $portnum\nWaiting for message (stat, date, time, {filter})/more/exit:\n"
-
-
-#python3 theServer.py "http://ns-mn1.cse.nd.edu/cse20289-fa24/hw03/data-all.json" "$portnum" | tee temp_py.txt &
-#echo $outputpy
-#'Server started successfully - listening on Port "$portnum"!' Wating for a new command
-
+make clean > /dev/null 2>&1
 
